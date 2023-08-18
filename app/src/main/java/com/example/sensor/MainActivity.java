c67222a53f51e441ce22434e213c175655949418
YoonGyu-Sun <YoonGyu-Sun>
2023-08-18 18:14:20 +0900

    음 이건 이제 manifests에 값 3개 넣고 build에 google fit 실행까지 넣어두고 권한부여까지 한 상황임. 근데 그 이후에 권한 하나 더있고 구글 계정 연동까지 해야함

--- app/src/main/java/com/example/sensor/MainActivity.java
+++ app/src/main/java/com/example/sensor/MainActivity.java
@@ -1,54 +1,79 @@
 package com.example.sensor;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.example.sensor.databinding.ActivityMainBinding;
 
 import java.security.Permission;
-
 public class MainActivity extends Activity {
 
-    private ActivityMainBinding binding; // 바인딩 객체 선언
+
+    private static final int MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION = 1;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater()); // 바인딩 초기화
-        setContentView(binding.getRoot()); // 바인딩한 뷰를 액티비티 콘텐츠 뷰로 설정
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        // 바인딩 객체를 통해 XML에 정의된 뷰 요소에 접근
         binding.button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent;
-                intent = new Intent(MainActivity.this, FirstActivity.class);
-                startActivity(intent);
-
-                int valueFromJava = 12345; // 예시로 설정한 값
-                intent.putExtra("value", valueFromJava); // 값을 인텐트에 추가
-                startActivity(intent);
-
+                checkAndRequestPermission(); // 버튼을 누를 때 권한 확인 및 요청
             }
         });
 
         binding.button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent;
-                intent = new Intent(MainActivity.this, FirstActivity.class);
-                startActivity(intent);
+                checkAndRequestPermission(); // 버튼을 누를 때 권한 확인 및 요청
             }
         });
 
-        binding.button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요", Toast.LENGTH_SHORT).show();
+        // ... button2와 button3의 클릭 리스너 등을 설정
+    }
+
+    private void checkAndRequestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)
+                != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.ACTIVITY_RECOGNITION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION);
+        } else {
+            // 권한이 이미 부여된 경우 MainActivity를 보여줌
+            Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults.length > 1 && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                // 권한이 부여되었을 때 MainActivity를 보여줌
+                Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+                startActivity(intent);
+            } else {
+                // 권한이 거부되었을 때 처리
+                Toast.makeText(this, "권한이 거부되었습니다.", Toast.LENGTH_SHORT).show();
             }
-        });
+        }
     }
 }
