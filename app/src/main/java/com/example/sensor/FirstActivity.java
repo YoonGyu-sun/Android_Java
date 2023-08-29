f4fd5eab3e3c7cf9479c637c6c3a01e4456bf917
YoonGyu-Sun <YoonGyu-Sun>
2023-08-29 12:54:14 +0900

    계정, 권한까지 완료, 헬스 커넥트 연결해서 각각의 권한 얻어오고 데이터 값 불러오기

--- app/src/main/java/com/example/sensor/FirstActivity.java
+++ app/src/main/java/com/example/sensor/FirstActivity.java
@@ -1,34 +1,32 @@
 package com.example.sensor;
-import android.util.Log;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.example.sensor.databinding.ActivityFirstBinding;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
-import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
-import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.request.DataReadRequest;
 import com.google.android.gms.fitness.result.DataReadResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
 
 import java.util.concurrent.TimeUnit;
+
 
 public class FirstActivity extends Activity {
 
     private ActivityFirstBinding binding;
+    private static final String TAG = "FirstActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,19 +34,8 @@ public class FirstActivity extends Activity {
         binding = ActivityFirstBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Intent에서 값 받아오기
-        int valueFromIntent = getIntent().getIntExtra("value", 0); // 기본값은 0
-        TextView valueTextView = binding.valueTextView; // XML의 TextView에 대한 바인딩
-        valueTextView.setText("Value: " + valueFromIntent);
 
-        binding.button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FirstActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        ///////////////////////////////////////// 구글 핏 누적 걸음 수 데이터 읽어오는 코드
         // Google Fit API 코드 추가
         readCumulativeStepCount();
     }
@@ -57,6 +44,7 @@ public class FirstActivity extends Activity {
         // Create a FitnessOptions with the desired data type and read access
         FitnessOptions fitnessOptions = FitnessOptions.builder()
                 .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
                 .build();
 
         // Get the Google Fit account
@@ -66,7 +54,7 @@ public class FirstActivity extends Activity {
         long endTimeMillis = System.currentTimeMillis();  // Current time
         long startTimeMillis = endTimeMillis - TimeUnit.DAYS.toMillis(7);  // 7 days ago
 
-        Fitness.getHistoryClient(this, account)
+        Task<DataReadResponse> tag = Fitness.getHistoryClient(this, account)
                 .readData(new DataReadRequest.Builder()
                         .read(DataType.TYPE_STEP_COUNT_DELTA)
                         .setTimeRange(startTimeMillis, endTimeMillis, TimeUnit.MILLISECONDS)
@@ -92,5 +80,95 @@ public class FirstActivity extends Activity {
                     }
                 });
 
+
+        binding.button5.setOnClickListener(v -> {
+                Intent intent = new Intent(FirstActivity.this, MainActivity.class);
+                startActivity(intent);
+            });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////// 중간에 들어가있ㄷ너 코드,
+
+
+
+
+//        // Intent에서 값 받아오기
+//        int valueFromIntent = getIntent().getIntExtra("value", 0); // 기본값은 0
+//        TextView valueTextView = binding.valueTextView; // XML의 TextView에 대한 바인딩
+//        valueTextView.setText("Value: " + valueFromIntent);
--- app/src/main/java/com/example/sensor/MainActivity.java
+++ app/src/main/java/com/example/sensor/MainActivity.java
@@ -1,26 +1,51 @@
 package com.example.sensor;
 
+import static com.google.android.gms.wearable.DataMap.TAG;
+
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-
-
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+// fitness 승인범위
+
 
 import com.example.sensor.databinding.ActivityMainBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.tasks.Task;
 
-import java.security.Permission;
+
 public class MainActivity extends Activity {
 
-    private static final int MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION = 1;
 
+    // 지금은 recognition만 허용을 해줬다 manifest는 아니고 mainactivity에서만
+    private static final int MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION = 1;
+    private static final int RC_SIGN_IN = 123;
     private ActivityMainBinding binding;
+    // 구글 로그인
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private void googleLogIn() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+    /// 구글 로그인
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +64,49 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 checkAndRequestPermission(); // 버튼을 누를 때 권한 확인 및 요청
+
             }
+
         });
 
-        // ... button2와 button3의 클릭 리스너 등을 설정
+        // 구글 로그인
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+
+
+
+            }
+        });
+        /// 구글 로그인
     }
 
+
+    // 구글 로그인
+    private void signIn() {
+        googleLogIn();
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+    /// 구글 로그인
+
+
+    // 구글 로그인
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+//         updateUI(account);
+
+    }
+/// 구글 로그인
+
+    // if 만약에 권한설정이 안되었거나
     private void checkAndRequestPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)
                 != PackageManager.PERMISSION_GRANTED ||
@@ -58,6 +120,9 @@ public class MainActivity extends Activity {
                     MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION);
         } else {
             // 권한이 이미 부여된 경우 MainActivity를 보여줌
+
+            //여기다가 이제 구글 로그인 연동을 해보면될듯 그리고 허락되면 intent해라
+
             Intent intent = new Intent(MainActivity.this, FirstActivity.class);
             startActivity(intent);
         }
@@ -69,6 +134,9 @@ public class MainActivity extends Activity {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
                     grantResults.length > 1 && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
                 // 권한이 부여되었을 때 MainActivity를 보여줌
+
+                //여기다가 이제 구글 로그인 연동을 해보면될듯 그리고 허락되면 intent해라/
+                signIn();
                 Intent intent = new Intent(MainActivity.this, FirstActivity.class);
                 startActivity(intent);
             } else {
@@ -77,4 +145,33 @@ public class MainActivity extends Activity {
             }
         }
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Intent intent = new Intent(MainActivity.this, FirstActivity.class); // 여기서 FirstActivity 대신 원하는 Activity를 넣어주세요.
+            startActivity(intent);
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
 }
